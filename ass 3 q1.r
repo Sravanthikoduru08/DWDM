d2c079dac1ea7e8c8e72d7ace98e0150d15c5eea
Sravanthikoduru08 <161932236+Sravanthikoduru08@users.noreply.github.com>
2026-08-05 22:20:46 +0530

    Add files via upload

---
+++ ass 3 q1.r
@@ -0,0 +1,10 @@
+photo <- matrix(c(18,22,20,
+                  2,28,40,
+                  20,10,40),
+                nrow = 3,
+                byrow = TRUE)
+
+colnames(photo) <- c("A","B","C")
+rownames(photo) <- c("5-6","7-8","9-10")
+
+photo
